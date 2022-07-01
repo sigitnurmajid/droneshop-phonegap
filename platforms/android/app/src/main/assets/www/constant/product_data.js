bb83c0bdcb37cde4ab40387134b91ccb80527cba
sigit nurmajid <sigitnurmajid32@gmail.com>
2022-07-01 22:27:56 +0700

    first commit

---
+++ platforms/android/app/src/main/assets/www/constant/product_data.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    "id": 1,
+    "img": "2ae419b6-7579-4f95-83eb-7bacb1cababc.jpg",
+    "name": "Happymodel Mobula6 65mm",
+    "price": "1,870,000",
+    "logo": "happymodel.png",
+    "mul_img": [
+      "2ae419b6-7579-4f95-83eb-7bacb1cababc.jpg",
+      "71388a34-cd7e-466b-b1fc-1ad28898e2fc.jpg",
+      "c4844c13-3a08-4a5d-b167-e741b28671da.jpg"
+    ],
+    "sizes": [
+      "65 mm",
+      "75 mm",
+      "85 mm",
+    ],
+  },
+  {
+    "id": 2,
+    "img": "2d8fe630-f513-47e8-b588-f4725ec20225.jpg",
+    "name": "GEPRC CineLog 25 4S 2.5",
+    "price": "2,770,199",
+    "logo": "geprc.png",
+    "mul_img": [
+      "2d8fe630-f513-47e8-b588-f4725ec20225.jpg",
+      "a94df2d2-6f33-4a14-a196-025e7c42d104.jpg",
+      "d3b194a8-6e6d-4218-971a-323be3d8f93f.jpg"
+    ],
+    "sizes": [
+      "65 mm",
+      "75 mm",
+      "85 mm",
+    ],
+  },
+  {
+    "id": 3,
+    "img": "07fb78ff-d2ac-4230-a763-c6c26bc90691.jpg",
+    "name": "GEPRC ROCKET Plus 112mm 2 Inch 4S",
+    "price": "5,146,864",
+    "logo": "geprc.png",
+    "mul_img": [
+      "07fb78ff-d2ac-4230-a763-c6c26bc90691.jpg",
+      "97a7d668-9e7f-4d1f-9e57-b198957619e1.jpg",
+      "ea3ee4bb-862e-47fc-88ce-9e9926aec928.jpg"
+    ],
+    "sizes": [
+      "65 mm",
+      "75 mm",
+      "85 mm",
+    ],
+  },
+  {
+    "id": 4,
+    "img": "deaccffb-851a-4ceb-8ce0-b89f182f1cdd.jpg",
+    "name": "EMAX Tinyhawk III 76mm 1S ",
+    "price": "2,026,580",
+    "logo": "emax.jpg",
+    "mul_img": [
+      "deaccffb-851a-4ceb-8ce0-b89f182f1cdd.jpg",
+      "e437ced6-7a34-44f6-8691-b3207c0ba1fd.jpg",
+      "ea09054d-e8ec-492a-b8ca-cf7fb6829a2f.jpg"
+    ],
+    "sizes": [
+      "65 mm",
+      "75 mm",
+      "85 mm",
+    ],
+  },
+  {
+    "id": 5,
+    "img": "964d9cd7-9e04-40c4-9357-7bedbea71aa6.jpg",
+    "name": "Geprc Cinelog35 2.20461 142mm",
+    "price": "6,205,427",
+    "logo": "geprc.png",
+    "mul_img": [
+      "964d9cd7-9e04-40c4-9357-7bedbea71aa6.jpg",
+      "1276f6ef-6c7e-4e6c-9448-91f1e783de0b.jpg",
+      "2123cc1c-7834-44ed-a77b-276267a0fec1.jpg"
+    ],
+    "sizes": [
+      "65 mm",
+      "75 mm",
+      "85 mm",
+    ],
+  }
+]
+
+export default products
